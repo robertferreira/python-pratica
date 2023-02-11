01bf0e4412c540d52c1881ec753355fbb7a8952f
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-11 12:18:58 -0300

    add

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n1 = 85
+
+print('O dobro de {} vale {}'.format(n1, n1*2))
+print('O triplo de {} vale {}'.format(n1, n1*3))
+print('O a raiz quadrada de {} vale {}'.format(n1, round(n1**(1/2), 2)))
