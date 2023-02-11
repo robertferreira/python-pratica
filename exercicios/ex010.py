03ead4a93e4010648286aa5ffe030c5a8b34e497
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-11 12:51:53 -0300

    add

---
+++ exercicios/ex010.py
@@ -0,0 +1,4 @@
+n1 = 19.88
+dolar = n1 / 5.22
+
+print('Com R${} você pode comprar US${}'.format(n1,round(dolar,2)))
---
+++ exercicios/ex016.py
@@ -0,0 +1,4 @@
+import math
+n1 = 3.14
+
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(n1,math.trunc(n1)))
